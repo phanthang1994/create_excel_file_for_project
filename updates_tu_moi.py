4613b7013825aed6ada89648e97f1b48d9182673
phanx <phanxuanthang20@gmail.com>
2023-08-27 23:31:05 +0700

    git tu_moi updates_tu_moi.py

--- updates_tu_moi.py
+++ updates_tu_moi.py
@@ -17,20 +17,21 @@ formatted_datetime = current_datetime.strftime('%d%m%y%H%M%S')
 files = os.listdir(folder_path)
 
 # Create a new Excel workbook and add a worksheet
-output_path = 'tu_moi_info.xlsx'
+output_path = 'tu_moi_info_updates.xlsx'
 workbook = xlsxwriter.Workbook(output_path)
 worksheet = workbook.add_worksheet()
 
 # Add headers
-worksheet.write('A1', 'name')
-worksheet.write('B1', 'image')
-worksheet.write('C1', 'tu_loai')
-worksheet.write('D1', 'phien_am')
-worksheet.write('E1', 'vi_du')
-worksheet.write('F1', 'audio')
-worksheet.write('G1', 'che_tu')
-worksheet.write('H1', 'cau_truc_cau')
-worksheet.write('I1', 'chu_de_id')
+worksheet.write('A1', 'id')
+worksheet.write('B1', 'name')
+worksheet.write('C1', 'image')
+worksheet.write('D1', 'tu_loai')
+worksheet.write('E1', 'phien_am')
+worksheet.write('F1', 'vi_du')
+worksheet.write('G1', 'audio')
+worksheet.write('H1', 'che_tu')
+worksheet.write('I1', 'cau_truc_cau')
+worksheet.write('J1', 'chu_de_id')
 
 
 # Loop through the files and extract name and extension
@@ -40,20 +41,20 @@ for row_num, file in enumerate(files, start=1):
         filename, extension = os.path.splitext(file)
 
         # Construct the new filename using formatted datetime
-        worksheet.write(row_num, 0, filename)
-        worksheet.write(row_num, 1, f"tu_moi-{filename}-{formatted_datetime}{extension}")
+        worksheet.write(row_num, 1, filename)
+        worksheet.write(row_num, 2, f"tu_moi-{filename}-{formatted_datetime}{extension}")
         new_filename = f"tu_moi-{filename}-{formatted_datetime}{extension}"
         new_file_path = os.path.join(folder_path, new_filename)
         os.rename(file_path, new_file_path)
 
 folder_path = './lesson_audio'
-output_path = 'tu_moi_info.xlsx'
+output_path = 'tu_moi_info_updates.xlsx'
 
 for row_num, file in enumerate(files, start=1):
     file_path = os.path.join(folder_path, file)
     if os.path.isfile(file_path):
         filename, extension = os.path.splitext(file)
-        worksheet.write(row_num, 5, f"tu_moi-{filename}-{formatted_datetime}{extension}")
+        worksheet.write(row_num, 6, f"tu_moi-{filename}-{formatted_datetime}{extension}")
         new_filename = f"tu_moi-{filename}-{formatted_datetime}{extension}"
         new_file_path = os.path.join(folder_path, new_filename)
         os.rename(file_path, new_file_path)
